80026cf6e439175bcb030086e04c943434ecaa3b
Raheem Orekoya <tundeorekoya1@gmail.com>
2024-06-01 15:51:30 +0100

    added scrollToTop Button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sideNav = document.querySelector("nav")
 const hamburger = document.querySelector(".hamburger")
 const closeBtn = document.querySelector(".close-btn")
+const scrollToTopButton = document.querySelector(".scroll-to-top-btn")
 
 hamburger.addEventListener("click", () => {
    sideNav.classList.add("active")
@@ -8,7 +9,11 @@ hamburger.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
    sideNav.classList.remove("active")
 })
+scrollToTopButton.addEventListener("click", () => {
+   window.scrollTo(0,0)
+})
 
 window.addEventListener("scroll", () => {
    sideNav.classList.remove("active", scrollY > 20)
+   scrollToTopButton.classList.toggle("active", scrollY > 500)
 })
